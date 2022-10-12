632ea1a03d3584fc213d568304b950f58e7a58a3
austinjyoung <89602020+austinjyoung@users.noreply.github.com>
2022-10-12 15:15:08 -0500

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,6 +1,6 @@
 # austinyoung:ajythw
-#Prints numbers ranging from 1 to 100
-#But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”
+# Prints numbers ranging from 1 to 100
+# But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”
 
 for i in range(1, 101):
     if i % 3 != 0 and i % 5 != 0:
