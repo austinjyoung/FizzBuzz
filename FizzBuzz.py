f1a4de190f63dc61616bb6737768ad706b166800
austinjyoung <89602020+austinjyoung@users.noreply.github.com>
2022-10-12 15:01:12 -0500

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,7 @@
 for i in range(100):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    if i % 3 == 0 and i % 5 != 0:
-        print("Fizz")
-    if i % 5 == 0 and i % 3 != 0:
-        print("Buzz")
+  if i % 3 == 0 and i % 5 == 0:
+      print("FizzBuzz")
+  if i % 3 == 0 and i % 5 != 0:
+      print("Fizz")
+  if i % 5 == 0 and i % 3 != 0:
+      print("Buzz")
